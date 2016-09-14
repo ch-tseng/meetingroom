efc21f255bacec069afc259f22451296021bb255
ch-tseng <ch.tseng@sunplusit.com>
2016-09-14 17:52:37 +0800

    Update

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+sudo umount -f /captured
 cd /home/pi/meetingroom
+sudo mount -t cifs -o gid=1000,uid=1000,username='ch.tseng',password='2laiuool@e020770E' //172.30.16.231/meetingroom$ /captured/
 python3 main.py
